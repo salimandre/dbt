1a1ceaabe71e57bb718c149d645fd56e14a994ac
Gautier Loveiko <gautier@MBP-de-Gautier.lan>
2024-05-19 20:47:59 +0200

    minor

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,7 +7,10 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table'),
+    alias='my_first_model'
+}}
 
 with source_data as (
 
